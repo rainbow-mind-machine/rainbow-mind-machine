f18ea7ed99065c8a322b123b3f9bca69de46cf76
Charles Reid <charlesreid1@gmail.com>
2015-02-22 04:03:18 -0800

    updating sheep. in the midst of tweet method of sheep.

--- rainbowmindmachine/Sheep.py
+++ rainbowmindmachine/Sheep.py
@@ -18,23 +18,89 @@ class Sheep(object):
     * Inner loop - tweet from tweet queue
     """
     def __init__(self,json_file):
+
+        # this is where we should 
+        # initialize the Twitter 
+        # API instance using params
+        # found in the json file.
+        with open(json_file,'r') as f:
+            self.params = json.load(f)
+
+        self.twitter_api_init()
+
+
+    def twitter_api_init(self):
+        # use self.params
+        # (also check Flickr photobots
+        #  for which library works for
+        #  media posts...)
         pass
 
+
     def perform_action(self,action,**kwargs):
+
         if action=='dummy':
             self.dummy(**kwargs)
+
         elif action=='echo':
             self.echo(**kwargs)
 
+        elif action=='tweet':
+            self.tweet(**kwargs)
+
+
     def dummy(self,**kwargs):
         pass
+
 
     def echo(self,**kwargs):
         print "Hello world!"
 
 
+    def tweet(self,**kwargs):
+        """
+        Keyword arguments:
+
+        inner_sleep     Inner loop sleep time (1 s)
+        outer_sleep     Outer loop sleep time (10 s)
+        publish         Actually publish (False)
+        """
+
+        # --------------------------
+        # Process parameters
+
+        tweet_params = kwargs
+
+        # Default parameter values
+        defaults['inner_sleep'] = 1.0
+        defaults['outer_sleep'] = 10.0
+        defaults['publish'] = False
+
+        # populate missing params with default values
+        for dk in defaults.keys():
+            if dk not in kwargs.keys():
+                tweet_params[dk] = defaults[dk]
+
+        # --------------------------
+        # The Real McCoy
+
+        while True:
+
+            try:
+                
+                # Outer loop
+                twitter_queue = self.populate_queue()
+
+                for tweet in twitter_queue:
+
+
+            pass:
+                # oops!
+                print "Encountered an exception. Continuing..."
+                time.sleep(
 
 
     def populate_queue(self):
         pass
+
 
