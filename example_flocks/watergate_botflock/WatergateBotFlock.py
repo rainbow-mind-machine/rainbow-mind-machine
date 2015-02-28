c6896ed0494e3031885ff2058ba6da880290b238
Charles Reid <charlesreid1@gmail.com>
2015-02-27 20:39:03 -0800

    adding watergate bot flock to examples

---
+++ example_flocks/watergate_botflock/WatergateBotFlock.py
@@ -0,0 +1,17 @@
+import rainbowmindmachine as rmm
+import logging
+
+
+def setup():
+    k = rmm.TxtKeymaker()
+    k.make_keys('poems/')
+    
+def run():
+    sh = rmm.Shepherd('keys/',sheep_class=rmm.QueneauSheep)
+
+    sh.perform_action('tweet',{'publish':False})
+    #sh.perform_pool_action('tweet',{'publish':False})
+
+if __name__=="__main__":
+    run()
+
