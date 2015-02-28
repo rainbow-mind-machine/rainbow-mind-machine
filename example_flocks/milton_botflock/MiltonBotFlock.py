a6848d22e00302a2b7eac84f241428305c8015c4
Charles Reid <charlesreid1@gmail.com>
2015-02-27 20:38:50 -0800

    removing cruft

--- example_flocks/milton_botflock/MiltonBotFlock.py
+++ example_flocks/milton_botflock/MiltonBotFlock.py
@@ -9,13 +9,6 @@ def setup():
 def run():
     sh = rmm.Shepherd('keys/',sheep_class=rmm.PoemSheep)
 
-    # # First, test that the Shepherd can get the 
-    # # flock of Sheep up and running.
-    # sh.perform_action('echo')
-
-    # Next, we need to test the Sheep ability
-    # to construct (and print) tweets from 
-    # the poem files.
     #sh.perform_action('tweet',{'publish':False})
     sh.perform_pool_action('tweet',{'publish':False})
 
