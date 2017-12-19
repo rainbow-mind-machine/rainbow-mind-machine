ab556d86cc16626ca46590029bccc20a245d1e83
Chaz Reid <charlesreid1@Gmail.com>
2017-12-19 14:04:24 -0800

    one action (pool)

--- example_flocks/ginsberg_botflock/GinsbergBotFlock.py
+++ example_flocks/ginsberg_botflock/GinsbergBotFlock.py
@@ -1,17 +1,13 @@
 import rainbowmindmachine as rmm
 import logging
 
-
 def setup():
     k = rmm.TxtKeymaker()
     k.make_keys('poems/')
     
 def run():
     sh = rmm.Shepherd('keys/',sheep_class=rmm.PoemSheep)
-
-    #sh.perform_action('tweet',{'publish':False})
     sh.perform_pool_action('tweet',{'publish':False})
 
 if __name__=="__main__":
     setup()
-
