285cccb2d7a6cac11ed81463a87049c1de874a99
Charles Reid <charlesreid1@gmail.com>
2018-04-02 18:01:45 -0700

    add poem bot flock example

---
+++ example_flocks/poem_botflock/PoemBotFlock.py
@@ -0,0 +1,36 @@
+import rainbowmindmachine as rmm
+import logging
+
+"""
+Poem Bot Flock Example
+
+This example shows how to create a bot flock 
+from a pile of text files (poems).
+
+Put text files into the poems directory.
+
+Run the setup() method once to create the keys,
+then run the run() method to fake tweet.
+
+Concepts covered:
+    - text file keymaker
+    - running bot actions in parallel
+"""
+
+def setup():
+    # create a keymaker that creates one key per text file
+    k = rmm.TxtKeymaker()
+
+    # create keys from text files in poems/
+    k.make_keys('poems/')
+    
+def run():
+    # create a shepherd looking for 
+    sh = rmm.Shepherd('keys/',sheep_class=rmm.QueneauSheep)
+
+    # have all bots tweet in parallel
+    sh.perform_pool_action('tweet',{'publish':False})
+
+if __name__=="__main__":
+    setup()
+    run()
