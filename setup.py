bf00a61b4849cbfdaa5efa7da0c76d1f984668c3
Charles Reid <charlesreid1@gmail.com>
2018-04-08 00:15:56 -0700

    bump version to 0.3

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
 setup(name='rmm',
-      version='0.2',
+      version='0.3',
       description='An extendable framework for running Twitter bot flocks.',
       url='http://github.com/charlesreid1/rainbow-mind-machine',
       author='charlesreid1',
