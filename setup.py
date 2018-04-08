ba88d6f4c32de9bdf49171289a3a7d21963a5bb7
Charles Reid <charlesreid1@gmail.com>
2018-04-08 01:18:34 -0700

    update setup.py

--- setup.py
+++ setup.py
@@ -1,6 +1,31 @@
 from setuptools import setup
 
-setup(name='rmm',
+# see http://python-packaging.readthedocs.io/en/latest/minimal.html
+# and https://packaging.python.org/guides/migrating-to-pypi-org/#uploading
+# 
+#   $ python setup.py register
+#   $ python setup.py sdist
+# 
+# add following to ~/.pypirc:
+# 
+# [distutils]
+# index-servers =
+#     pypi
+# 
+# [pypi]
+# username:charlesreid1
+# password:YOURPASSWORDHERE
+#
+#   $ python setup.py sdist upload
+#
+# test it out with virtualenv:
+#
+#   $ virtualenv vp && cd vp
+#   $ source bin/activate
+#   $ bin/pip install rainbowmindmachine
+
+
+setup(name='rainbowmindmachine',
       version='0.4',
       description='An extendable framework for running Twitter bot flocks.',
       url='http://github.com/charlesreid1/rainbow-mind-machine',
