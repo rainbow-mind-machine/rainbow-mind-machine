2c2fb12f337601c2987dcc83488654abfc6ad0cb
Charles Reid <charlesreid1@gmail.com>
2018-04-08 00:56:48 -0700

    worked out image file name logic in example Milton Bot Flock, now just need to add flock action

--- example_flocks/milton_botflock/MiltonBotFlock.py
+++ example_flocks/milton_botflock/MiltonBotFlock.py
@@ -4,10 +4,35 @@ import logging
 def setup():
     k = rmm.TxtKeymaker()
     k.make_keys('poems/')
+
+def set_img():
+    """
+    Running perform_action() with the change_image action
+    requires each bot to have a change_image param set.
     
-def run():
+    The change_image action takes one parameter, 'image',
+    which is the path to the image.
+    
+    Before we call perform_action(), we have to 
+    set the 'image' attritube for each sheep.
+    """
+    sh = rmm.Shepherd('keys/',
+                       sheep_class=rmm.PoemSheep)
+
+    for sheep in sh.all_sheep:
+        poem_file = sheep.params['file']
+        img_file = re.sub('poems','img',poem_file)
+        img_file = re.sub('txt','jpg',img_file)
+        sheep.params['image'] = img_file
+
+    # TODO: add set_image perform_action
+
+def tweet():
     sh = rmm.Shepherd('keys/',sheep_class=rmm.PoemSheep)
     sh.perform_pool_action('tweet',{'publish':False})
 
 if __name__=="__main__":
-    run()
+    setup()
+    #set_img()
+    #tweet()
+
