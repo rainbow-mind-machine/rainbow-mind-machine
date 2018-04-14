dc26d415b83748975415e7098aef00097aa8deda
Charles Reid <charlesreid1@gmail.com>
2018-04-13 23:37:54 -0700

    add required packages to setup.py

--- setup.py
+++ setup.py
@@ -34,11 +34,14 @@ from setuptools import setup
 
 
 setup(name='rainbowmindmachine',
-      version='0.5.2',
+      version='0.6',
       description='An extendable framework for running Twitter bot flocks.',
       url='http://charlesreid1.github.io/rainbow-mind-machine',
       author='charlesreid1',
       author_email='charles@charlesreid1.com',
+      install_requires = ['simplejson==3.13',
+          'oauth2>=1.5','python-twitter==3.4.1',
+          'TwitterAPI==2.4.6','TextBlob==0.15'],
       license='MIT',
       packages=['rainbowmindmachine'],
       zip_safe=False)
