2a07ddccabc3cfdea6f6c0c8fb843957b752ce2d
Charles Reid <charlesreid1@gmail.com>
2018-04-28 15:34:31 -0700

    removing test

--- test/keymaker_test/test_txtkeymaker.py
+++
@@ -1,8 +0,0 @@
-import rainbowmindmachine as rmm
-import os
-from nose.tools import *
-from .apikeys import *
-
-keymaker = rmm.TxtKeymaker()
-keymaker.make_keys('txt/')
-
--- test/shepherdsheep_test/README.md
+++
@@ -1,9 +0,0 @@
-# Shepherd/Sheep Tests
-
-To run these tests, just run them with python:
-
-```
-python test_shepherdsheep.py
-```
-
-
