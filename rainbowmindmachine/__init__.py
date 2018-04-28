da51bec9bd393a5317509e63679e9a8c0e722c72
Charles Reid <charlesreid1@gmail.com>
2018-04-28 01:24:29 -0700

    Get version information from rainbowmindmachine/__init__.py

--- rainbowmindmachine/__init__.py
+++ rainbowmindmachine/__init__.py
@@ -1,9 +1,9 @@
-from .Lumberjack import *
-from .Keymaker import *
-from .Shepherd import *
-from .Sheep import *
-from .PoemSheep import *
-from .PhotoADaySheep import *
-from .queneau import DialogueAssembler
-from .QueneauSheep import *
-
+__version__ = "0.7"
+#from .Lumberjack import *
+#from .Keymaker import *
+#from .Shepherd import *
+#from .Sheep import *
+#from .PoemSheep import *
+#from .PhotoADaySheep import *
+#from .queneau import DialogueAssembler
+#from .QueneauSheep import *
--- setup.py
+++ setup.py
@@ -1,16 +1,19 @@
 from setuptools import setup
+from rainbowmindmachine import __version__
 
-setup(name='rainbowmindmachine',
-      version='0.6.5',
-      description='An extendable framework for running Twitter bot flocks.',
-      url='https://pages.charlesreid1.com/b-rainbow-mind-machine',
-      author='charlesreid1',
-      author_email='charles@charlesreid1.com',
-      install_requires = ['simplejson==3.13',
-                          'oauth2>=1.5',
-                          'python-twitter==3.4.1',
-                          'TextBlob==0.15'],
-      license='MIT',
-      packages=['rainbowmindmachine'],
-      zip_safe=False)
+with open('requirements.txt') as f:
+    required = [x for x in f.read().splitlines() if not x.startswith("#")]
+
+setup(  name='rainbowmindmachine',
+        version=__version__,
+        description='An extendable framework for running Twitter bot flocks.',
+        url='https://pages.charlesreid1.com/b-rainbow-mind-machine',
+        author='charlesreid1',
+        author_email='charles@charlesreid1.com',
+        test_suite='nose.collector',
+        tests_require=['nose'],
+        license='MIT',
+        packages=['rainbowmindmachine'],
+        install_requires=required,
+        zip_safe=False)
 
