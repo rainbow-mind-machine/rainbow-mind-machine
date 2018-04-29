b25265e6d7a07b82215b33ef472e5cec329b9494
Charles Reid <charlesreid1@gmail.com>
2018-04-28 21:43:58 -0700

    update tests to properly capture/log output from logging module

--- tests/test_shepherd.py
+++ tests/test_shepherd.py
@@ -1,7 +1,7 @@
 import rainbowmindmachine as rmm
 from unittest import TestCase
-import os
-from .utils import captured_output
+import sys, os, subprocess, logging
+from .utils import captured_output, LoggingContext
 
 class TestShepherd(TestCase):
 
@@ -13,14 +13,24 @@ class TestShepherd(TestCase):
     def test_shepherd(self):
         # This should create a flock named 
         # "Anonymous Flock of Cowards"
-        with captured_output() as (out, err):
-            s = rmm.Shepherd(
-                flock_name='test flock',
-                json_key_dir='tests/keys',
-                sheep_class=rmm.Sheep
-            )
-        output = out.getvalue().strip()
-        self.assertIn('Creating flock', output)
+        logger = logging.getLogger('rainbowmindmachine')
+        h = logging.StreamHandler(sys.stdout)
+
+        # this with block ensures logs print to stdout
+        with LoggingContext(logger, level=logging.INFO, handler=h, close=True):
+
+            # this with block ensures we capture stdout
+            with captured_output() as (out, err):
+
+                # Create the Shepherd (this starts the logger)
+                s = rmm.Shepherd(
+                    flock_name='test flock',
+                    json_key_dir='tests/keys',
+                    sheep_class=rmm.Sheep
+                )
+
+            stderrlog = err.getvalue().strip()
+            self.assertIn('Creating flock', stderrlog)
 
     def test_queneau_shepherd(self):
         #log_file = 'tests/test_queneau_shepherd.log'
--- tests/utils.py
+++ tests/utils.py
@@ -1,6 +1,7 @@
 import sys
 from contextlib import contextmanager
 from io import StringIO
+import logging
 
 @contextmanager
 def captured_output():
@@ -11,4 +12,49 @@ def captured_output():
         yield sys.stdout, sys.stderr
     finally:
         sys.stdout, sys.stderr = old_out, old_err
+
+
+
+# https://docs.python.org/3/howto/logging-cookbook.html
+
+class LoggingContext(object):
+    def __init__(self, logger, level=None, handler=None, close=True):
+        self.logger = logger
+        self.level = level
+        self.handler = handler
+        self.close = close
+
+    def __enter__(self):
+        if self.level is not None:
+            self.old_level = self.logger.level
+            self.logger.setLevel(self.level)
+        if self.handler:
+            self.logger.addHandler(self.handler)
+
+    def __exit__(self, et, ev, tb):
+        if self.level is not None:
+            self.logger.setLevel(self.old_level)
+        if self.handler:
+            self.logger.removeHandler(self.handler)
+        if self.handler and self.close:
+            self.handler.close()
+        # implicit return of None => don't swallow exceptions
+
+
+# # Example using the logging context handler:
+# 
+# if __name__ == '__main__':
+#     logger = logging.getLogger('foo')
+#     logger.addHandler(logging.StreamHandler())
+#     logger.setLevel(logging.INFO)
+#     logger.info('1. This should appear just once on stderr.')
+#     logger.debug('2. This should not appear.')
+#     with LoggingContext(logger, level=logging.DEBUG):
+#         logger.debug('3. This should appear once on stderr.')
+#     logger.debug('4. This should not appear.')
+#     h = logging.StreamHandler(sys.stdout)
+#     with LoggingContext(logger, level=logging.DEBUG, handler=h, close=True):
+#         logger.debug('5. This should appear twice - once on stderr and once on stdout.')
+#     logger.info('6. This should appear just once on stderr.')
+#     logger.debug('7. This should not appear.')
 
