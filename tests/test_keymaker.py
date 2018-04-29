31b794d4eb3313e2885a5a58a4f429f9359f704e
Charles Reid <charlesreid1@gmail.com>
2018-04-28 17:17:29 -0700

    expanding keymaker tests

--- tests/test_keymaker.py
+++ tests/test_keymaker.py
@@ -1,28 +1,83 @@
 import rainbowmindmachine as rmm
 from unittest import TestCase
-from .utils import captured_output
 import os
+from .utils import captured_output
 
 thisdir = os.path.abspath(os.path.dirname(__file__))
 
 class TestKeymaker(TestCase):
 
-    def test_default_keymaker(self):
-        keymaker = rmm.Keymaker()
+    def test_default_keymaker_apikeys_env(self):
+        """
+        Test ability to create single key using consumer token from environment vars
+        """
+        os.environ['CONSUMER_TOKEN'] = 'AAAAA'
+        os.environ['CONSUMER_TOKEN_SECRET'] = 'BBBBB'
 
-    def test_txt_keymaker(self):
-        keymaker = rmm.TxtKeymaker()
+        keymaker = rmm.Keymaker()
+        keymaker.set_apikeys_env()
+
+        with captured_output() as (out, err):
+            keymaker.make_a_key({
+                'name' : 'test_default_keymaker_apikeys_env',
+                'json' : 'test_default_keymaker_apikeys_env.json'
+            },
+            interactive=False)
+
+        os.environ['CONSUMER_TOKEN'] = ''
+        os.environ['CONSUMER_TOKEN_SECRET'] = ''
+
+        output = out.getvalue().strip()
+        self.assertIn('Successfully exported a key bundle', output)
+
+    def test_default_keymaker_apikeys_file(self):
+        """
+        Test ability to create single key using consumer token/secret from JSON file
+        """
+        keymaker = rmm.Keymaker()
 
         apikeys = os.path.join(thisdir,'apikeys.json')
         keymaker.set_apikeys_file(apikeys)
 
-        txtdir = os.path.join(thisdir,'txt/')
         with captured_output() as (out, err):
-            keymaker.make_keys(txtdir,interactive=False)
+            keymaker.make_a_key({
+                'name' : 'test_default_keymaker_apikeys_file',
+                'json' : 'test_default_keymaker_apikeys_file.json'
+            },
+            interactive=False)
+
         output = out.getvalue().strip()
         self.assertIn('Did not export a key bundle', output)
+        self.assertIn('Successfully exported a key bundle', output)
+
+    def test_default_keymaker_apikeys_dict(self):
+        """
+        Test ability to create single key using consumer token/secret from dictionary
+        """
+        keymaker = rmm.Keymaker()
+
+        keymaker.set_apikeys_dict({ 
+            'consumer_token': 'AAAAA',
+            'consumer_token_secret': 'BBBBB'
+        })
+
+        with captured_output() as (out, err):
+            keymaker.make_a_key({
+                'name' : 'test_default_keymaker_apikeys_dict',
+                'json' : 'test_default_keymaker_apikeys_dict.json'
+            },
+            interactive=False)
+
+        output = out.getvalue().strip()
+        self.assertIn('Did not export a key bundle', output)
+        self.assertIn('Successfully exported a key bundle', output)
 
     def test_files_keymaker(self):
+        """
+        Test the FilesKeymaker, which makes one key
+        for each file with a given extension 
+        in a given directory.
+        """
         keymaker = rmm.FilesKeymaker()
 
         apikeys = os.path.join(thisdir,'apikeys.json')
@@ -33,11 +88,22 @@ class TestKeymaker(TestCase):
             keymaker.make_keys(filesdir,interactive=False)
         output = out.getvalue().strip()
         self.assertIn('Did not export a key bundle', output)
+        self.assertIn('Successfully exported a key bundle', output)
 
+    def test_txt_keymaker(self):
+        """
+        Test the TxtKeymaker, which makes one key
+        for each .txt file in a given directory.
+        """
+        keymaker = rmm.TxtKeymaker()
 
-#keymaker = rmm.Keymaker()
-#keymaker.make_a_key({
-#        'name':'charlesreid1',
-#        'json':'keys/charlesreid1.json'
-#    })
+        apikeys = os.path.join(thisdir,'apikeys.json')
+        keymaker.set_apikeys_file(apikeys)
+
+        txtdir = os.path.join(thisdir,'txt/')
+        with captured_output() as (out, err):
+            keymaker.make_keys(txtdir,interactive=False)
+        output = out.getvalue().strip()
+        self.assertIn('Did not export a key bundle', output)
+        self.assertIn('Successfully exported a key bundle', output)
 
