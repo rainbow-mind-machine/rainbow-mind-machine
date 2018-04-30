ebb17522ce4f1e6e3c19ee9959a859933d43f082
Charles Reid <charlesreid1@gmail.com>
2018-04-29 21:22:52 -0700

    clarifying comments. deal with keys dir and keys json as variables.

--- tests/test_keymaker.py
+++ tests/test_keymaker.py
@@ -6,21 +6,33 @@ from .utils import captured_output
 thisdir = os.path.abspath(os.path.dirname(__file__))
 
 class TestKeymaker(TestCase):
+    """
+    Test the Keymaker class.
+
+    This focuses on testing the constructors
+    and the API key initialization process.
+    """
 
     @classmethod
     def setUpClass(self):
-        self.keys_dir = 'tests/keys'
+        self.keys_dir = os.path.join( thisdir, "test_keymaker_keys" )
+        self.api_keys = os.path.join( thisdir, "apikeys.json" )
+
+        # Note: this gives the same outcome as 
+        # hard-coding the path "tests"
 
     def test_default_keymaker_apikeys_env(self):
         """
         Test ability to create single key using consumer token from environment vars
         """
+        keymaker = rmm.Keymaker()
+
+        # Set application API keys
         os.environ['CONSUMER_TOKEN'] = 'AAAAA'
         os.environ['CONSUMER_TOKEN_SECRET'] = 'BBBBB'
-
-        keymaker = rmm.Keymaker()
         keymaker.set_apikeys_env()
 
+        # Create bot API keys
         with captured_output() as (out, err):
             keymaker.make_a_key(
                 name = 'test_default_keymaker_apikeys_env',
@@ -28,9 +40,11 @@ class TestKeymaker(TestCase):
                 keys_out_dir=self.keys_dir,
                 interactive=False)
 
+        # Clean up
         os.environ['CONSUMER_TOKEN'] = ''
         os.environ['CONSUMER_TOKEN_SECRET'] = ''
 
+        # Assert
         output = out.getvalue().strip()
         self.assertIn('Did not export a key bundle', output)
 
@@ -40,9 +54,11 @@ class TestKeymaker(TestCase):
         """
         keymaker = rmm.Keymaker()
 
+        # Set application API keys
         apikeys = os.path.join(thisdir,'apikeys.json')
         keymaker.set_apikeys_file(apikeys)
 
+        # Create bot API keys
         with captured_output() as (out, err):
             keymaker.make_a_key(
                 name = 'test_default_keymaker_apikeys_file',
@@ -50,6 +66,7 @@ class TestKeymaker(TestCase):
                 keys_out_dir=self.keys_dir,
                 interactive=False)
 
+        # Assert
         output = out.getvalue().strip()
         self.assertIn('Did not export a key bundle', output)
 
@@ -59,11 +76,13 @@ class TestKeymaker(TestCase):
         """
         keymaker = rmm.Keymaker()
 
+        # Set application API keys
         keymaker.set_apikeys_dict({ 
             'consumer_token': 'AAAAA',
             'consumer_token_secret': 'BBBBB'
         })
 
+        # Create bot API keys
         with captured_output() as (out, err):
             keymaker.make_a_key(
                 name = 'test_default_keymaker_apikeys_dict',
@@ -71,8 +90,26 @@ class TestKeymaker(TestCase):
                 keys_out_dir = self.keys_dir,
                 interactive = False)
 
+        # Assert
         output = out.getvalue().strip()
         self.assertIn('Did not export a key bundle', output)
+
+    @classmethod
+    def tearDownClass(self):
+        # Remove the keys directory we created
+        subprocess.call(['rm','-rf',self.keys_dir])
+
+
+
+class TestFilesKeymaker(TestCase):
+    """
+    Test file-based keymaker classes.
+    """
+
+    @classmethod
+    def setUpClass(self):
+        self.keys_dir = os.path.join( thisdir, "test_keymaker_files_keys" )
+        self.api_keys = os.path.join( thisdir, "apikeys.json" )
 
     def test_files_keymaker(self):
         """
@@ -82,14 +119,19 @@ class TestKeymaker(TestCase):
         """
         keymaker = rmm.FilesKeymaker()
 
+        # Set application API keys
         apikeys = os.path.join(thisdir,'apikeys.json')
         keymaker.set_apikeys_file(apikeys)
 
+        # Create bot API keys
+        # from files in filesdir
         filesdir = os.path.join(thisdir,'files/')
         with captured_output() as (out, err):
-            keymaker.make_keys(filesdir,
-                keys_out_dir=self.keys_dir,
-                interactive=False)
+            keymaker.make_keys( filesdir,
+                                keys_out_dir=self.keys_dir,
+                                interactive=False)
+
+        # Assert
         output = out.getvalue().strip()
         self.assertIn('Did not export a key bundle', output)
 
@@ -100,16 +142,21 @@ class TestKeymaker(TestCase):
         """
         keymaker = rmm.TxtKeymaker()
 
+        # Set application API keys
         apikeys = os.path.join(thisdir,'apikeys.json')
         keymaker.set_apikeys_file(apikeys)
 
+        # Create bot API keys
         txtdir = os.path.join(thisdir,'txt/')
         with captured_output() as (out, err):
             keymaker.make_keys(txtdir,
                 keys_out_dir=self.keys_dir,
                 interactive=False)
+
+        # Assert
         output = out.getvalue().strip()
         self.assertIn('Did not export a key bundle', output)
+
 
     @classmethod
     def tearDownClass(self):
