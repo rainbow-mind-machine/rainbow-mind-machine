849248e7d9ee0d33da17b7468964e1cff20c8223
Charles Reid <charlesreid1@gmail.com>
2018-05-14 20:12:45 -0700

    hard coding library names, so dumb

--- setup.py
+++ setup.py
@@ -1,5 +1,6 @@
 from setuptools import setup
 from rainbowmindmachine import __version__
+from pip.req import parse_requirements
 
 with open('requirements.txt') as f:
     required = [x for x in f.read().splitlines() if not x.startswith("#")]
@@ -14,6 +15,10 @@ setup(  name='rainbowmindmachine',
         tests_require=['nose'],
         license='MIT',
         packages=['rainbowmindmachine'],
-        install_requires=required,
+        install_requires=['simplejson>=3.13',
+                'oauth2>=1.5',
+                'python-twitter>=3.4.1',
+                'TextBlob>=0.15',
+                'oauth2client>=3.0.0'],
         zip_safe=False)
 
