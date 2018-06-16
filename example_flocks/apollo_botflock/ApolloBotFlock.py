b0eb35be205a84452253b606fbb0cfefa2d48a10
Charles Reid <charlesreid1@gmail.com>
2018-06-15 20:26:14 -0700

    fix apollo example

--- example_flocks/apollo_botflock/ApolloBotFlock.py
+++ example_flocks/apollo_botflock/ApolloBotFlock.py
@@ -1,14 +1,15 @@
 import rainbowmindmachine as rmm
-import logging
 
 def setup():
     k = rmm.TxtKeymaker()
+    k.set_apikeys_file('apikeys.json')
     k.make_keys('data/')
     
 def run():
-    sh = rmm.Shepherd('keys/',sheep_class=rmm.QueneauSheep)
-    sh.perform_action('tweet',{'publish':False})
-    #sh.perform_pool_action('tweet',{'publish':False})
+    sh = rmm.Shepherd('keys/',
+            sheep_class=rmm.QueneauSheep
+    )
+    sh.perform_pool_action('tweet',{'publish':False})
 
 if __name__=="__main__":
-    run()
+    setup()
