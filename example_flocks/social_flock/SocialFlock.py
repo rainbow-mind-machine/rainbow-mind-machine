3233a0a6c86bccebd6c27e10fff2811d35a9dc05
Charles Reid <charlesreid1@gmail.com>
2018-06-17 23:08:19 -0700

    reorganize/fix social flock

--- example_flocks/social_flock/SocialFlock.py
+++ example_flocks/social_flock/SocialFlock.py
@@ -17,31 +17,32 @@ def setup():
             keys_out_dir = 'keys'
         )
 
+def shepherd():
+    sh = rmm.Shepherd( 
+            json_keys_dir = 'keys/',
+            flock_name = 'simple flock',
+            sheep_class = TestSocialSheep
+    )
+    return sh
 
 def tweet():
-    sh = rmm.Shepherd( json_keys_dir = 'keys/',
-                       flock_name = 'simple flock',
-                       sheep_class = TestSocialSheep
-                    )
-
-    sh.perform_parallel_action('tweet',
+    sh = shepherd()
+    sh.perform_parallel_action(
+            'tweet',
             publish = False,
             inner_sleep = 5,
             outer_sleep = 10 
     )
 
-
 def favorite():
-    sh = rmm.Shepherd( json_keys_dir = 'keys/',
-                       flock_name = 'simple flock',
-                       sheep_class = TestSocialSheep
-                    )
-
-    sh.perform_parallel_action('favorite_toilet',
+    sh = shepherd()
+    sh.perform_parallel_action(
+            'favorite',
             sleep = 60,
             search_term = '#rainbowmindmachine'
     )
 
 if __name__=="__main__":
-    tweet()
+    setup()
+    #favorite()
 
