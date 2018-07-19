87833936913c11ae94bff18f807e57a663253bcc
your name here <charlesreid1@gmail.com>
2018-07-18 23:04:45 -0700

    Update: move twitter keymaker to rainbow-mind-machine

--- rainbowmindmachine/__init__.py
+++ rainbowmindmachine/__init__.py
@@ -1,3 +1,4 @@
+from .TwitterKeymaker import *
 from .TwitterShepherd import *
 from .TwitterSheep import *
 from .SocialSheep import *
@@ -6,22 +7,3 @@ from .PhotoADaySheep import *
 from .queneau import DialogueAssembler
 from .QueneauSheep import *
 
-import boringmindmachine as bmm
-# alias
-class TwitterKeymaker(bmm.TwitterKeymaker):
-    """
-    To change the way the GithubKeymaker works,
-    make changes in boringmindmachine, which
-    defines all Keymakers.
-    """
-    pass
-class FilesKeymaker(bmm.FilesKeymaker):
-    """
-    See boringmindmachine
-    """
-    pass
-class TxtKeymaker(bmm.TxtKeymaker):
-    """
-    See boringmindmachine
-    """
-    pass
