e01c5af547c5b566cb4099dd25106887b250593c
Charles Reid <charlesreid1@gmail.com>
2018-09-08 09:53:06 -0700

    :facepalm: all we needed was assertLogs() and this logging+tests mess just disappeared

--- tests/test_shepherd.py
+++ tests/test_shepherd.py
@@ -2,17 +2,16 @@ import rainbowmindmachine as rmm
 from unittest import TestCase
 import logging
 import sys, os, subprocess, logging
+import json
 from .utils import captured_output, LoggingContext
+import nose
 
+from io import StringIO
+from unittest.mock import patch
 
 """
 Test Shepherd classes
 """
-
-
-console = logging.StreamHandler()
-console.setLevel(logging.DEBUG)
-logging.getLogger('').addHandler(console)
 
 
 thisdir = os.path.abspath(os.path.dirname(__file__))
@@ -30,35 +29,39 @@ class TestShepherd(TestCase):
         the output of the Keymaker.
         We create a bare minimum fake key.
         """
-
         # These parameters go in a JSON file and are given to the Sheep on creation.
         # The name of the bot key JSON file is set by the 'json' item below.
         # 
         # Here, we create two fake bot keys for two bots
         # 
         fake_bot_keys = [{
-            'consumer_token' : 'AAAAA',
-            'consumer_token_secret' : 'BBBBB',
-            'oauth_token' : 'CCCCC',
-            'oauth_token_secret' : 'DDDDD',
+            'consumer_token' : 'AAAAA1',
+            'consumer_token_secret' : 'BBBBB1',
+            'oauth_token' : 'CCCCC1',
+            'oauth_token_secret' : 'DDDDD1',
             'user_id' : '00000000',
-            'screen_name' : 'EEEEE',
-            'name' : 'FFFFF',
-            'json' : 'GGGGG.json'
+            'screen_name' : 'EEEEE1',
+            'name' : 'FFFFF1',
+            'json' : 'GGGGG1.json'
         },
         {
-            'consumer_token' : 'AAAAA',
-            'consumer_token_secret' : 'BBBBB',
-            'oauth_token' : 'CCCCC',
-            'oauth_token_secret' : 'DDDDD',
+            'consumer_token' : 'AAAAA2',
+            'consumer_token_secret' : 'BBBBB2',
+            'oauth_token' : 'CCCCC2',
+            'oauth_token_secret' : 'DDDDD2',
             'user_id' : '00000000',
-            'screen_name' : 'EEEEE',
-            'name' : 'FFFFF',
-            'json' : 'GGGGG.json'
+            'screen_name' : 'EEEEE2',
+            'name' : 'FFFFF2',
+            'json' : 'GGGGG2.json'
         }]
 
-        mkdir_cmd = ['mkdir', '-p', self.keys_dir]
-        subprocess.call(mkdir_cmd)
+        if not os.path.exists(self.keys_dir):
+            os.mkdir(self.keys_dir)
+
+        for bot_key in fake_bot_keys:
+            ofile = os.path.join(self.keys_dir, bot_key['json'])
+            with open(ofile,'w') as f:
+                json.dump(bot_key,f)
 
 
     def test_shepherd(self):
@@ -84,32 +87,30 @@ class TestShepherd(TestCase):
         Function _setup_sheep() iterates over each key processed
         by _setup_keys() and makes one Sheep from each key.
         """
-        logger = logging.getLogger('rainbowmindmachine')
-        h = logging.StreamHandler(sys.stdout)
+        #################################
+        ##### https://stackoverflow.com/a/31281467
+        #####
+        #with patch('sys.stdout', new=StringIO()) as fakeOutput:
+        #    print('hello world')
+        #    self.assertEqual(fakeOutput.getvalue().strip(), 'hello world')
 
-        # this "with" block ensures logs print to stdout
-        with LoggingContext(logger, level=logging.INFO, handler=h, close=True):
+        #    # this "with" block ensures we capture stdout
+        #    with captured_output() as (out, err):
 
-            # this "with" block ensures we capture stdout
-            with captured_output() as (out, err):
+        # Create the Shepherd (this starts the logger)
+        s = rmm.TwitterShepherd(
+            json_keys_dir = 'tests/shepherd_test_keys',
+            sheep_class = rmm.TwitterSheep
+        )
 
-                # Create the Shepherd (this starts the logger)
-                s = rmm.TwitterShepherd(
-                    flock_name = 'test flock',
-                    json_keys_dir = 'tests/shepherd_test_keys',
-                    sheep_class = rmm.Sheep
-                )
-
-            stderrlog = err.getvalue().strip()
-            self.assertIn('Creating flock', stderrlog)
-            self.assertIn('Successfully created', stderrlog)
-            self.assertEqual('test flock',s.name)
+        self.assertLogs('Creating flock')
+        self.assertLogs('Successfully created')
 
 
     def test_queneau_shepherd(self):
         log_file = 'tests/test_queneau_shepherd.log'
-        sh = rmm.TwitterShepherd(json_keys_dir = 'keys/',
-                          flock_name = 'My Apollo Flock',
+        sh = rmm.TwitterShepherd(json_keys_dir = self.keys_dir,
+                          flock_name = 'My Fake Flock',
                           sheep_class = rmm.QueneauSheep,
                           log_file = log_file)
 
@@ -124,8 +125,8 @@ class TestShepherd(TestCase):
         """
         Clean up the fake bot keys
         """
-        rmdir_cmd = ['rm', '-rf', self.keys_dir]
-        subprocess.call(rmdir_cmd)
+        rm_keys_dir = ['rm','-fr',self.keys_dir]
+        subprocess.call(rm_keys_dir)
 
         rmlog_cmd = ['rm', '-rf', 'rainbowmindmachine.log']
         subprocess.call(rmlog_cmd)
